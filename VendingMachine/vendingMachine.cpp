79ba6bcdb356db4b0e0bbb9c4c758fc269827a8f
PravendraRana <pravendra.rana@cctech.co.in>
2023-01-05 13:50:28 +0530

    updated

--- VendingMachine/vendingMachine.cpp
+++ VendingMachine/vendingMachine.cpp
@@ -4,6 +4,9 @@ VenderMachine::VenderMachine()
 {
     totalAmount = 0.0;
     productPrice = 0.0;
+    noOfCola = 10;
+    noOfChips = 10;
+    noOfCandy = 10;
 }
 
 bool VenderMachine::isValidCoin(string coin)
@@ -11,6 +14,11 @@ bool VenderMachine::isValidCoin(string coin)
     if(coin == "nickels" || coin == "dimes" || coin == "quarter")
        return true;
     return false;
+}
+
+bool VenderMachine::soldOut(int noOfProduct)
+{
+    return noOfProduct <= 0;
 }
 
 void VenderMachine::acceptCoin(int argc, char* argv[])
@@ -32,13 +40,59 @@ void VenderMachine::acceptCoin(int argc, char* argv[])
 
 void VenderMachine::selectProduct()
 {   
-    string item;
-    cout << "SELECT ITEM:\n-> cola    -> chips\n-> candy  -> return-coin" << endl;
+    int item;
+    cout << "SELECT OPTION:\n1. cola    2. chips\n3. candy  4. return-coin" << endl;
     cin >> item;
-    if(item == "return-coin")
-       returnCoin();
-    else
-       productPrice += product[item];
+    switch (item)
+    {
+    case 1:
+        if(soldOut(noOfCola))
+        {
+            cout << "SOLD OUT!" << endl;
+            cout << "Please choose different option" << endl;
+            selectProduct();
+        }
+        else {
+            productPrice += product["cola"];
+            noOfCola--;
+        }
+        break;
+    case 2:
+        if(soldOut(noOfChips))
+        {
+            cout << "SOLD OUT!" << endl;
+            cout << "Please choose different option" << endl;
+            selectProduct();
+        }
+        else {
+            productPrice += product["chips"];
+            noOfChips--;
+        }
+        break;
+    case 3:
+        if(soldOut(noOfCandy))
+        {
+            cout << "SOLD OUT!" << endl;
+            cout << "Please choose different option" << endl;
+            selectProduct();
+        }
+        else {
+            productPrice += product["candy"];
+            noOfCandy--;
+        }
+        break;
+    case 4:
+        returnCoin();
+        return;
+    default:
+        cout << "INVALID OPTION! Please try again." << endl;
+        selectProduct();
+        break;
+    }
+    // if(item == "return-coin")
+    //    returnCoin();
+    // else
+    //    productPrice += product[item];
     if(totalAmount < productPrice)
     {
         cout << "Insufficient coins!\nInserted money : " << totalAmount
@@ -47,7 +101,7 @@ void VenderMachine::selectProduct()
         addCoin();
     }
     else
-        makeChange(item);
+        makeChange();
 }
 
 void VenderMachine::addCoin()
@@ -55,23 +109,29 @@ void VenderMachine::addCoin()
     string coin;
     cout << "INSERT COIN!" << endl;
     cin >> coin;
-    totalAmount += coins[coin];
+    if(isValidCoin(coin))
+        totalAmount += coins[coin];
+    else
+    {
+        cerr << "INVALID COINS, PLEASE COLLECT IT AND TRY AGAIN WITH VALID COINS." << endl;
+        addCoin();
+    }
     productPrice = 0.0;
     selectProduct();
 }
 
-void VenderMachine::makeChange(string item)
+void VenderMachine::makeChange()
 {
     if(totalAmount == productPrice)
     {
         totalAmount = 0.0;
-        cout << "Collect your " << item << endl
+        cout << "Collect your product"<< endl
              << "THANK YOU." << endl
              << "----------" << endl;
     }
     else if(totalAmount > productPrice)
     {
-        cout << "Collect your change : " << totalAmount - productPrice<< " & collect your " << item << endl
+        cout << "Collect your change : " << totalAmount - productPrice<< " & collect your product"<< endl
              << "THANK YOU." << endl
              << "----------" << endl;
         totalAmount = 0.0;
--- VendingMachine/vendingMachine.h
+++ VendingMachine/vendingMachine.h
@@ -14,20 +14,22 @@ private:
     unordered_map<string, float> coins = {{"nickels", 0.05},
                                {"dimes", 0.10},
                                {"quarter", 0.25}};
+    int noOfCola;
+    int noOfChips;
+    int noOfCandy;
     float productPrice;
     unordered_map<string, float> product = {{"cola", 1.00},
                                            {"chips", 0.50},
                                            {"candy", 0.65}};
     bool isValidCoin(string);
+    bool soldOut(int);
 public:
     VenderMachine();
     void acceptCoin(int, char*[]);
     void selectProduct();
     void addCoin();
-    void makeChange(string);
+    void makeChange();
     void returnCoin();
-    void soldOut();
-    void exactChange();
 };
 
 #endif
